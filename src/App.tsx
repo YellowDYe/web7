2c0887175639e847a7f4b20f51f3035df096dd77
YellowDYe <yellowdyemx@gmail.com>
2026-08-19 14:27:36 -0600

    Add Weekly Menu page

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ import CustomerForgotPasswordPage from './customer/pages/CustomerForgotPasswordP
 import CustomerOrderPage from './customer/pages/CustomerOrderPage';
 import CustomerCartPage from './customer/pages/CustomerCartPage';
 import CustomerBlogPostPage from './customer/pages/CustomerBlogPostPage';
+import CustomerWeeklyMenuPage from './customer/pages/CustomerWeeklyMenuPage';
 import Admin from './pages/Admin';
 import ForcePasswordChange from './pages/ForcePasswordChange';
 import ForgotPassword from './pages/ForgotPassword';
@@ -125,6 +126,7 @@ function App() {
                   />
                   <Route path="/cart" element={<CustomerCartPage />} />
                   <Route path="/blog/:slug" element={<CustomerBlogPostPage />} />
+                  <Route path="/menu" element={<CustomerWeeklyMenuPage />} />
                   <Route
                     path="/checkout"
                     element={
